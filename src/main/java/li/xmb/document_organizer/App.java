6cff144859c8eb92c9b14a5df521daeeb5c2e0b1
XMBomb <11milan@gmx.ch>
2016-05-19 16:51:20 +0200

    Initial Commit

---
+++ src/main/java/li/xmb/document_organizer/App.java
@@ -0,0 +1,13 @@
+package li.xmb.document_organizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
